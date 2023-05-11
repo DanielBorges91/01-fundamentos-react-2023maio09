a1b046ff80f66743a405a588115ca68c9f501815
Daniel <danprog91@gmail.com>
2023-05-10 22:08:15 -0300

    Propiedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { Post } from "./Post";
 export function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet 
+        consectetur adipisicing elit. Excepturi, architecto laudantium ea eum
+        magni sed veritatis non eos, sequi neque minima voluptatibus cumque
+        maiores sint debitis doloribus perspiciatis numquam. Sunt."
+      />
+      <Post 
+        author="Daniel Borges"
+        content="Um novo post muito legal."
+      />
     </>
   )
 }
