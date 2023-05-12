0ab2861cda4494cf00894f4f8552a25dde209229
Daniel <danprog91@gmail.com>
2023-05-11 23:50:06 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Siderbar";
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      />
-      <Post 
-        author="Daniel Borges"
-        content="Um novo post muito legal."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Diego Fernandes"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+          <Post 
+          author="Daniel Borges"
+          content="Um novo post muito legal."
+          />
+        </main>
+      </div>
     </div>
   )
 }
